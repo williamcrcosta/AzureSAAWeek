d7195031e5b5948e6443b8953875090cc6211ae5
William Costa <william-crcosta@outlook.com>
2024-05-06 00:30:13 -0300

    Add file Deploy Landing-Zone Azure SAA Week

--- provider.tf
+++ provider.tf
@@ -1,8 +1,15 @@
+# provider "azurerm" block
+# Esse bloco de código configura o provedor de recursos do Azure
+# para o Terraform.
+# Aqui estão as configurações do provedor de recursos do Azure.
+# A id da assinatura é a UUID da assinatura do Azure.
+# A id do cliente é a UUID do aplicativo do Azure AD.
+# A chave secreta do cliente é a chave secreta do aplicativo do Azure AD.
+# A id do locatário é a UUID do locatário do Azure AD.
 provider "azurerm" {
   features {}
-  //subscription_id = "99433210-484b-482d-af20-1fabe24fc6fc"
-  subscription_id = "482bbf03-c333-4929-b7db-aebdc8b145fa"
-  client_id       = "ccc7c710-30e5-4526-8175-740919c70ea4" # Display name  "studytfe-spn01"
-  client_secret   = "nWF8Q~E-blUWXguVL4o~zfh~mYj6INOxJCTy2bOd"
-  tenant_id       = "959d0b5c-960d-48aa-aed9-8ffe74e05790"
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
 }
