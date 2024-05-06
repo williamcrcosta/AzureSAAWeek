4f6e2479b34bafb0c04b27cced7c57cfd7cd234d
William Costa <william-crcosta@outlook.com>
2024-05-06 00:27:23 -0300

    Add file Deploy Landing-Zone Azure SAA Week

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+  //subscription_id = "99433210-484b-482d-af20-1fabe24fc6fc"
+  subscription_id = "482bbf03-c333-4929-b7db-aebdc8b145fa"
+  client_id       = "ccc7c710-30e5-4526-8175-740919c70ea4" # Display name  "studytfe-spn01"
+  client_secret   = "nWF8Q~E-blUWXguVL4o~zfh~mYj6INOxJCTy2bOd"
+  tenant_id       = "959d0b5c-960d-48aa-aed9-8ffe74e05790"
+}
